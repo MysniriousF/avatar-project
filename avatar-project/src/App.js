bba77d54b2531976335c80b3299ba5351258ec0e
SnirNahum <snir.nahum@playermaker.com>
2021-07-26 09:28:58 +0300

    added avatar component

---
+++ avatar-project/src/App.js
@@ -0,0 +1,12 @@
+import './Components/Avatar/Avatar.js'
+import Avatar from './Components/Avatar/Avatar.js'
+
+function App() {
+  return (
+    <div>
+      <Avatar image = "https://lh3.googleusercontent.com/proxy/hpf48qnmOxYrhoACy-2sNvDCPO9NugW6UzolJj3ifYaxV1OdqiQm7GeJDcUHksP9wVRuQ1CXlP8kmSqEc9uoQ11s5IMFJm8"></Avatar>
+    </div>
+  );
+}
+
+export default App;
---
+++ avatar-project/src/Components/Avatar/Avatar.js
@@ -0,0 +1,4 @@
+
+export default function Avatar(props) {
+    return <img src={props.image} className = "Avatar"/>
+}
